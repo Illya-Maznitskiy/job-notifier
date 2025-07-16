9d9397945171c17632b4792232541c8fd7a3377d
Illia <illia.maznitskyi@gmail.com>
2025-07-16 15:19:21 +0200

    Update justjoin.py for single-responsibility logic

---
+++ fetchers/justjoin/justjoin.py
@@ -0,0 +1,17 @@
+from fetchers.justjoin.fetcher import fetch_jobs
+from fetchers.justjoin.save import save_jobs_to_json
+from logs.logger import logger
+
+
+async def run_fetch_and_save_jobs():
+    """
+    Fetch jobs asynchronously and save to JSON file.
+    """
+    jobs = await fetch_jobs()
+
+    if not jobs:
+        logger.info("no_jobs")
+    else:
+        save_jobs_to_json(jobs)
+
+    return jobs
