c4bb935b8f3a4954a7bafe023ee5095cb9eed00b
Illia <illia.maznitskyi@gmail.com>
2025-07-17 15:42:10 +0200

    Update justjoin.py saving logic

--- fetchers/justjoin/justjoin.py
+++ fetchers/justjoin/justjoin.py
@@ -1,5 +1,5 @@
 from fetchers.justjoin.fetcher import fetch_jobs
-from fetchers.justjoin.save import save_jobs_to_json
+from fetchers.save_jobs import save_jobs_to_json
 from logs.logger import logger
 
 
@@ -12,6 +12,6 @@ async def run_fetch_and_save_jobs():
     if not jobs:
         logger.info("no_jobs")
     else:
-        save_jobs_to_json(jobs)
+        save_jobs_to_json(jobs, "justjoin_jobs.json")
 
     return jobs
