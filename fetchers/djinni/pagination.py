d17153b83641e170b379d7059e68bcd5988dcfa8
Illia <illia.maznitskyi@gmail.com>
2025-07-17 15:08:55 +0200

    Update pagination.py djinni with better logging

--- fetchers/djinni/pagination.py
+++ fetchers/djinni/pagination.py
@@ -1,15 +1,16 @@
 from urllib.parse import urlencode, urlparse, parse_qs, urlunparse
 
+from logs.logger import logger
+
 
 def build_paginated_url(base_url: str, page_number: int):
     """Appends or updates the 'page' parameter in the URL."""
+    logger.info("-" * 60)
     url_parts = list(urlparse(base_url))
     query = parse_qs(url_parts[4])
     query["page"] = [str(page_number)]
     url_parts[4] = urlencode(query, doseq=True)
     final_url = urlunparse(url_parts)
 
-    print(
-        f"[build_paginated_url] Built URL for page {page_number}: {final_url}"
-    )
+    logger.info(f"Built URL for page {page_number}: {final_url}")
     return final_url
