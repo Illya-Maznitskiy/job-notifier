584b01b056cad098943a7690f7b7de2ba8dd6f1b
Illia <illia.maznitskyi@gmail.com>
2025-07-18 16:38:59 +0200

    Update main.py to send mail and run Telegram bot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
 
 from logs.logger import logger
+from notifier.email_sender import send_job_listings_email
+from notifier.telegram_bot import dp, bot
 from storage.combine_json import save_all_vacancies
 from utils.fetch_orchestrator import run_all_fetchers
 from utils.job_filter import filter_and_score_jobs_from_file
@@ -26,6 +28,24 @@ async def main():
     logger.info(
         f"Filtering done. {len(filtered_jobs)} jobs passed the threshold."
     )
+
+    # Send jobs to email
+    logger.info("Sending job listings via email...")
+    if send_job_listings_email():
+        logger.info("Email sent successfully.")
+    else:
+        logger.warning("Failed to send email.")
+
+    # Run Telegram bot
+    run_telegram = (
+        input("Do you want to run the Telegram bot? (y/n): ").strip().lower()
+    )
+    if run_telegram == "y":
+        logger.info("Starting Telegram bot...")
+        logger.info("Use /stop inside the bot to stop the bot.")
+        await dp.start_polling(bot)
+    else:
+        logger.info("Telegram bot was skipped by user.")
 
 
 if __name__ == "__main__":
