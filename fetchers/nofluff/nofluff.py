2fc607d35e612f2399506791f965bf77aacdfb78
Illia <illia.maznitskyi@gmail.com>
2025-08-06 16:17:48 +0200

    Remove jobs logging from nofluff.py

--- fetchers/nofluff/nofluff.py
+++ fetchers/nofluff/nofluff.py
@@ -21,9 +21,6 @@ async def run_fetch_and_save_jobs():
 
     jobs = await fetch_nofluff_jobs(NO_FLUFF_URL)
 
-    for i, job in enumerate(jobs, 1):
-        logger.info(f"{i}. {job}")
-
     save_jobs_to_json(jobs, filename="nofluff_jobs.json")
     logger.info("Job titles saved successfully.")
 
