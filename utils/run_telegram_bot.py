503496f9b176cffaa4aa2bbc8cfd9bf2f6e6b1f7
Illia <illia.maznitskyi@gmail.com>
2025-08-07 16:12:42 +0200

    Remove question from run_telegram_bot.py to run it directly for better Docker support

--- utils/run_telegram_bot.py
+++ utils/run_telegram_bot.py
@@ -3,14 +3,10 @@ from notifier.telegram.telegram_bot import start_bot
 
 
 async def run_telegram_bot(dp, bot):
-    """Ask user and run Telegram bot if confirmed."""
-    run_telegram = (
-        input("Do you want to run the Telegram bot? (y/n): ").strip().lower()
-    )
-
-    if run_telegram == "y":
+    """Run Telegram bot."""
+    try:
         logger.info("Starting Telegram bot...")
         logger.info("Use /stop inside the bot to stop the bot.")
         await start_bot()
-    else:
-        logger.info("Telegram bot was skipped by user.")
+    except Exception as e:
+        logger.error(f"Error while running Telegram bot: {e}", exc_info=True)
