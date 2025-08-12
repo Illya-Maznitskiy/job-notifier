b87d35d2519e0acce8f5448e29c702c1fff7c75d
Illia <illia.maznitskyi@gmail.com>
2025-08-12 21:29:08 +0200

    Update bulldog.py to save jobs to db

--- fetchers/bulldog/bulldog.py
+++ fetchers/bulldog/bulldog.py
@@ -1,7 +1,8 @@
 import asyncio
 
+from db.db import AsyncSessionLocal
 from fetchers.bulldog.fetcher import fetch_bulldog_jobs
-from fetchers.save_jobs import save_jobs_to_json
+from fetchers.save_jobs import save_jobs_to_db
 from logs.logger import logger
 
 
@@ -10,9 +11,14 @@ async def run_fetch_and_save_jobs():
     logger.info("Starting full fetch and save operation")
 
     jobs = await fetch_bulldog_jobs()
-    save_jobs_to_json(jobs, "bulldog_jobs.json")
 
-    logger.info("Fetch and save completed")
+    if not jobs:
+        logger.info("No jobs fetched from bulldog.")
+    else:
+        async with AsyncSessionLocal() as session:
+            await save_jobs_to_db(jobs, session)
+
+    logger.info("Bulldog job fetch process completed.")
 
     return jobs
 
