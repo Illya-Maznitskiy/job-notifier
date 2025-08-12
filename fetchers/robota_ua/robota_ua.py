1082d820bbaa5473b982d8c245f01e0a69e1977f
Illia <illia.maznitskyi@gmail.com>
2025-08-12 21:25:25 +0200

    Update robota_ua.py to save jobs to db

--- fetchers/robota_ua/robota_ua.py
+++ fetchers/robota_ua/robota_ua.py
@@ -1,7 +1,8 @@
 import asyncio
 
+from db.db import AsyncSessionLocal
 from fetchers.robota_ua.fetcher import fetch_robota_ua_jobs
-from fetchers.save_jobs import save_jobs_to_json
+from fetchers.save_jobs import save_jobs_to_db
 from logs.logger import logger
 
 
@@ -10,9 +11,14 @@ async def run_fetch_and_save_jobs():
     logger.info("Starting full fetch and save operation")
 
     jobs = await fetch_robota_ua_jobs()
-    save_jobs_to_json(jobs, "robota_ua.json")
 
-    logger.info("Fetch and save completed")
+    if not jobs:
+        logger.info("No jobs fetched from robota ua.")
+    else:
+        async with AsyncSessionLocal() as session:
+            await save_jobs_to_db(jobs, session)
+
+    logger.info("Robota ua job fetch process completed.")
 
     return jobs
 
