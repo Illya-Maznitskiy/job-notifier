77d472852686bb94458989d3453abd46e9bb8c34
Illia <illia.maznitskyi@gmail.com>
2025-08-25 16:35:39 +0200

    Update imports and logs in fastapi_app.py

--- src/utils/fastapi_app.py
+++ src/utils/fastapi_app.py
@@ -5,29 +5,31 @@ from fastapi import FastAPI
 import uvicorn
 
 from logs.logger import logger
-from src.utils.email_sender import send_job_listings_email
-from src.telegram import dp, bot
-from src.utils import run_all_fetchers
-from src.utils import run_telegram_bot
-
+from src.telegram.telegram_bot import dp, bot
+from src.utils.run_telegram_bot import run_telegram_bot
+from src.utils.job_loop import job_process_loop
 
 app = FastAPI()
 
 
 @app.get("/healthz")
 async def health_check():
+    logger.info("-" * 60)
     return {"status": "ok"}
 
 
 @app.on_event("startup")
 async def on_startup():
+    logger.info("-" * 60)
+    logger.info("Starting background job loop")
     # Start job processing loop in background
     asyncio.create_task(job_process_loop())
+
+    logger.info("Starting Telegram bot")
     # Start telegram bot (this will block so run it in background)
     asyncio.create_task(run_telegram_bot(dp, bot))
 
 
 if __name__ == "__main__":
-
     port = int(os.getenv("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
