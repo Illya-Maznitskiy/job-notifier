3bb84aa10aa65d533bb45d0b88dba5782cda247e
Illia <illia.maznitskyi@gmail.com>
2025-08-25 18:48:20 +0200

    Update main.py to run project correctly

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import os
-
 import uvicorn
 import asyncio
-
 from src.utils.fastapi_app import app
 from src.utils.job_loop import job_process_loop
 
 
-def main():
+async def main():
     """Start FastAPI server and background job loop"""
     # Start background job loop
     asyncio.create_task(job_process_loop())
 
     # Run FastAPI server
-    port = int(os.getenv("PORT", 8080))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    port = int(os.getenv("PORT", 8000))
+    config = uvicorn.Config(app, host="0.0.0.0", port=port)
+    server = uvicorn.Server(config)
+    await server.serve()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
