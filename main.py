1e5e9b402ce3330ccf49909f1bbf2e8f39a85b75
Illia <illia.maznitskyi@gmail.com>
2025-08-30 16:54:34 +0200

    Fix loop issue in main.py

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ from src.utils.job_loop import job_process_loop
 
 async def main():
     """Start FastAPI server and background job loop"""
-    # Start background job loop
-    # asyncio.create_task(job_process_loop())
-
     # Run FastAPI server
     port = int(os.getenv("PORT", 8000))
     config = uvicorn.Config(app, host="0.0.0.0", port=port)
