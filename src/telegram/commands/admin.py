9e560cbbef7f7d855bc09e0461f7585e76bbaf71
Illia <illia.maznitskyi@gmail.com>
2025-09-02 19:04:34 +0200

    Clean code in admin.py

--- src/telegram/commands/admin.py
+++ src/telegram/commands/admin.py
@@ -1,5 +1,3 @@
-import os
-
 from aiogram.filters import Command
 from aiogram import types
 
@@ -9,20 +7,24 @@ from src.telegram.bot_config import (
     dp,
     bot,
 )
+from src.telegram.job_utils import ADMIN_ID
 
 
 @dp.message(Command("stop"))
-async def cmd_stop(message: types.Message):
+async def cmd_stop(message: types.Message) -> None:
     """Stop the bot if user is admin."""
     logger.info("-" * 60)
     user_id = str(message.from_user.id)
 
-    if user_id != os.getenv("ADMIN_ID"):
+    if user_id != ADMIN_ID:
         logger.warning(f"Unauthorized stop attempt by user {user_id}")
         await message.answer("You are not allowed to stop the bot.")
         return
 
     logger.info(f"Admin {user_id} requested bot shutdown.")
     await message.answer("Bot is shutting down...")
-    await bot.session.close()
-    await dp.stop_polling()
+    try:
+        await bot.session.close()
+        await dp.stop_polling()
+    except Exception as e:
+        logger.error(f"Error shutting down bot: {e}")
