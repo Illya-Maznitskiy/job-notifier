348e096090b99a72979d0251d96ec7474054ca6f
Illia <illia.maznitskyi@gmail.com>
2025-09-04 18:10:30 +0200

    Add user.py to models

---
+++ src/db/models/user.py
@@ -0,0 +1,24 @@
+from sqlalchemy import (
+    String,
+    BigInteger,
+)
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.db.models.base import Base
+from src.db.models.user_job import UserJob
+from src.db.models.user_keyword import UserKeyword
+
+
+class User(Base):
+    """Telegram user info."""
+
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(BigInteger, unique=True, index=True)
+    username: Mapped[str] = mapped_column(String(100), nullable=True)
+
+    jobs: Mapped[list["UserJob"]] = relationship(back_populates="user")
+    keywords: Mapped[list["UserKeyword"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
