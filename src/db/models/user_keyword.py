be9031eeb672b04cc40c323fd10e4cd667d0a21e
Illia <illia.maznitskyi@gmail.com>
2025-09-04 18:09:35 +0200

    Add user_keyword.py to models

---
+++ src/db/models/user_keyword.py
@@ -0,0 +1,28 @@
+from sqlalchemy import (
+    String,
+    Integer,
+    ForeignKey,
+    UniqueConstraint,
+)
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.db.models.base import Base
+from src.db.models.user import User
+
+
+class UserKeyword(Base):
+    """Keywords for each user with weights."""
+
+    __tablename__ = "user_keywords"
+    __table_args__ = (
+        UniqueConstraint("user_id", "keyword", name="uix_user_keyword"),
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    keyword: Mapped[str] = mapped_column(String(100), nullable=False)
+    weight: Mapped[int] = mapped_column(
+        Integer, nullable=False
+    )  # positive or negative
+
+    user: Mapped["User"] = relationship(back_populates="keywords")
