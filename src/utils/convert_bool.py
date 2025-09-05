7f03637628bcbd0874854ae393d9f6ec4f541fa8
Illia <illia.maznitskyi@gmail.com>
2025-09-05 18:40:57 +0200

    Remove convert_bool.py

--- src/utils/convert_bool.py
+++
@@ -1,9 +0,0 @@
-def str_to_bool(s: str) -> bool:
-    """
-    Convert a string to a boolean.
-
-    Returns True if the string (case-insensitive) matches common truthy values:
-    'true', '1', 'yes', 'y', or 'on'. Otherwise, returns False.
-    """
-
-    return s.lower() in ("true", "1", "yes", "y", "on")
